b5154672f184bc9c65a35436eb060f6833a84f07
AryanKatariya <aryankatariya21@gmail.com>
2023-12-14 23:41:40 +0530

    14/12/23

---
+++ Python/array/13.commanChar.py
@@ -0,0 +1,15 @@
+def count(l1,l2):
+    l1,l2=len(n),len(m)
+    cnt=[[0 for i in range(l2+1)] for i in range(l1+1)]
+    for i in range(1,l1+1):
+        for j in range(1,l2+1):
+            if(n[i-1] == m[j-1]):
+                cnt[i][j] = 1 + cnt[i][j-1] + cnt[i-1][j]
+            else:
+                cnt[i][j] = cnt[i][j-1] + cnt[i-1][j] - cnt[i-1][j-1]
+    print(cnt[l1][l2])
+
+n="ABC"
+m="AB"
+
+count(n,m)
