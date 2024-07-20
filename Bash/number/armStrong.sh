023f9f5c59ed122ba0c78a78e5569d9fb51691fd
AryanKatariya <aryankatariya21@gmail.com>
2024-07-20 23:34:26 +0530

    armstrong

---
+++ Bash/number/armStrong.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+read -p "Enter A Number: " n
+
+arm=0
+temp=$n
+while [ "$n" -ne 0 ]; do
+	r=$((n % 10))
+	arm=$((arm + r * r * r))
+	n=$((n / 10))
+done
+
+echo $arm
+
+if [ $arm -eq "$temp" ]; then
+	echo "Armstrong"
+else
+	echo "Not Armstrong"
+fi
