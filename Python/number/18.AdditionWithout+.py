4fb46c3d5c56ae7869a523b1f9699a639fff924a
AryanKatariya <aryankatariya21@gmail.com>
2024-08-22 23:49:05 +0530

    +

---
+++ Python/number/18.AdditionWithout+.py
@@ -0,0 +1,13 @@
+def add_without_plus_operator(a, b):
+    while b != 0:
+        carry = a & b
+        a = a ^ b
+        b = carry << 1
+    
+    return a
+
+# Example usage
+num1 = 15
+num2 = 27
+result = add_without_plus_operator(num1, num2)
+print(f"The sum of {num1} and {num2} is: {result}")
